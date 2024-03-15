48fda01021f315132bf15272f4e26cabaee52938
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-15 09:32:05 +0200

    tasks table render on tasks page

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,9 @@
+import TasksTable from "@/components/TasksTable"
+
+export default function TasksPage() {
+    return (
+        <div className="flex flex-col min-h-screen justify-center items-center">
+            <TasksTable />
+        </div>
+    )
+}
