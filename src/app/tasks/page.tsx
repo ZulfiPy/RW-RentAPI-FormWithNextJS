49baa30a2c5177a3d4e2c10c8ce4795efba83cd2
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-17 13:18:37 +0200

    /tasks page created

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,9 +1,20 @@
-import TasksTable from "@/components/TasksTable"
+'use client';
+import TasksTable from "@/components/TasksTable";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function TasksPage() {
+    const router = useRouter();
+
     return (
-        <div className="flex flex-col min-h-screen justify-center items-center">
+        <section className="container flex flex-col items-center justify-center space-y-11">
+            <h1 className="text-3xl font-bold  mt-10">Tasks Table</h1>
             <TasksTable />
-        </div>
+            <Button
+                className="py-8"
+                onClick={() => router.push('/new-task')}>
+                Add a New Task
+            </Button>
+        </section>
     )
 }
