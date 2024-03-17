16b0593e87494538d25512037662742c2647b38f
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-17 13:17:47 +0200

    /new-task page created

--- src/app/new-task/page.tsx
+++ src/app/new-task/page.tsx
@@ -1,5 +1,10 @@
+import InputTaskForm from "@/components/InputTaskForm"
+
 export default function NewTaskPage() {
     return (
-        <>here will be located new task form</>
+        <section className="container flex flex-col items-center space-y-10">
+            <h1 className="text-3xl font-bold mt-10">New Task Form</h1>
+            <InputTaskForm />
+        </section>
     )
 }
