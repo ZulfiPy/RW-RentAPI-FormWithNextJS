765f7e5fc85a33b3b4387257f8f91dd77def6e43
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-22 10:28:26 +0200

    toaster added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import * as React from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "RW-Rent",
@@ -13,12 +16,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className="bg-white dark:bg-black">
-        <Header />
-        <main>
-          {children}
-        </main>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main>
+            {children}
+          </main>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
