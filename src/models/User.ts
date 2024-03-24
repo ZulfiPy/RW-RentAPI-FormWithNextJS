3fcf3b217bb23c569cb18fb14d52827e1f6b0d99
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-24 11:47:57 +0200

    User mongoose model

---
+++ src/models/User.ts
@@ -0,0 +1,49 @@
+import { Schema, models, model } from "mongoose";
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"]
+    },
+    isikukood: {
+        type: String,
+        unique: [true, "Isikukood already exists"],
+        required: [true, "Isikukood is required"]
+    },
+    birthDate: {
+        type: String,
+        required: [true, "Birth date is required"]
+    },
+    username: {
+        type: String,
+        unique: [true, "Username already exists"],
+        required: [true, "Username is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+}, {
+    timestamps: true
+});
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
