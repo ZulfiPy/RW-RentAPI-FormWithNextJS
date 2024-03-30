dd1e6490adbb99ba874ab024bccd41ae128a9688
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-30 17:22:54 +0200

    global Task type

---
+++ new-types.d.ts
@@ -0,0 +1,14 @@
+
+declare global {
+    interface Task {
+        _id: string,
+        title: string,
+        description: string,
+        priority: string,
+        status: string,
+        status: string,
+        createdAt: string
+    }
+}
+
+export default globalж
