8cf8c406a39f02aa8ae4c751fa177833755fc41a
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-30 12:36:42 +0200

    session based rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import HomeCarousel from "@/components/HomeModuleCarousel";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 
-export default function Home() {
+import Link from "next/link";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  // console.log(session)
+
   return (
-    <section className="container flex flex-col items-center justify-center min-h-screen mx-auto">
-      <HomeCarousel />
-    </section>
+    <section className="container flex flex-col items-center justify-center min-h-screen">
+      {session
+        ? <HomeCarousel />
+        : <Link
+          href="/api/auth/signin"
+          className="font-bold">
+          Welcome to the RW-Rent internal systems page.<br/>
+          If you wish to use inner system ,please sign in.
+        </Link>}
+    </section >
   );
 }
