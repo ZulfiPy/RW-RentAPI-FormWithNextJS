9827b0728efd3f41a2fa506960a3e5ffa02a0867
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-03-30 12:29:15 +0200

    POST request for new task adding

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,42 @@
+import connectDB from "@/config/database";
+import Task from "@/models/Task";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// GET /api/tasks
+export async function GET(req: Request, res: Response) {
+
+}
+
+// POST /api/tasks
+export async function POST(req: Request, res: Response) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return Response.json({ message: "You must be logged in" }, { status: 401 });
+    }
+
+    const data = await req.json();
+
+    try {
+        await connectDB();
+
+        const newTask = await new Task(data).save();
+
+        return Response.json(newTask, { status: 201 })
+    } catch (error) {
+        console.log('error occured while adding new task(POST request)', error);
+        return Response.json({ message: "Something went wrong" }, { status: 500 })
+    }
+}
+
+// PUT /api/tasks
+export async function PUT(req: Request, res: Response) {
+
+}
+
+// DELETE /api/tasks
+export async function DELETE(req: Request, res: Response) {
+
+}
