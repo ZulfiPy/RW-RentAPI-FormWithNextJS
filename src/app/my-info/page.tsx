aa3983ee0a8dad709c2f753ab401941fecbaf5de
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-06 12:58:02 +0300

    session added to to my-info page

--- src/app/my-info/page.tsx
+++ src/app/my-info/page.tsx
@@ -1,12 +1,5 @@
 'use client';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from "@/components/ui/card";
-
+import MyProfileCard from "@/components/MyProfileCard";
 import { useSession } from "next-auth/react";
 
 export default function MyInfo() {
@@ -15,29 +8,7 @@ export default function MyInfo() {
     return (
         <section className="container flex flex-col items-center justify-center min-h-screen">
             <div>
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Your Data</CardTitle>
-                        <CardDescription>Here is provided your user session data</CardDescription>
-                    </CardHeader>
-                    <CardContent className="flex flex-col space-y-3">
-                        <label>
-                            <span className="font-bold underline">ID:</span> {session?.user?.id}
-                        </label>
-                        <label>
-                            <span className="font-bold underline">Username:</span> {session?.user?.username}
-                        </label>
-                        <label>
-                            <span className="font-bold underline">Name:</span> {session?.user?.name}
-                        </label>
-                        <label>
-                            <span className="font-bold underline">Email:</span> {session?.user?.email}
-                        </label>
-                        <label>
-                            <span className="font-bold underline">Roles:</span> {!session?.user?.roles ? "No Roles" : "User role assigned"}
-                        </label>
-                    </CardContent>
-                </Card>
+                <MyProfileCard session={session as Session} />
             </div>
         </section>
     )
