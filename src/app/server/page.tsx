c63ab8b41ab7a4ce3290433aad8ce9a03d114780
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-06 12:57:11 +0300

    server page

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,7 +1,15 @@
-export default function ServerPage() {
+import { Button } from "@/components/ui/button"
+
+
+export default async function ServerPage() {
+
     return (
-        <section>
-            server page
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            <Button
+                type="button"
+            >
+                Server
+            </Button>
         </section>
     )
 }
