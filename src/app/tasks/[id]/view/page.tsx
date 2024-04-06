50556759445ec91fd2024715dd968bfa837b7045
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-06 12:55:17 +0300

    task view page by id

---
+++ src/app/tasks/[id]/view/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { fetchIndividualTask } from "@/utils/tasksRequests";
+import { useState, useEffect } from "react";
+import Spinner from "@/components/Spinner";
+import ViewTaskCard from "@/components/ViewTaskCard";
+
+const ViewTaskPage = ({ params }: { params: { id: string } }) => {
+    const { id } = params;
+    const [task, setTask] = useState<Task>();
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+
+        const fetchTaskData = async () => {
+            try {
+                const task = await fetchIndividualTask(params.id);
+                setTask(task);
+            } catch (error) {
+                console.log('error in useEffect', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        if (loading) {
+            fetchTaskData();
+        }
+
+    }, [id]);
+
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            {loading ?
+                <Spinner loading={loading} />
+                :
+                <ViewTaskCard task={task} id={id} />
+            }
+        </section>
+    )
+}
+
+export default ViewTaskPage;
