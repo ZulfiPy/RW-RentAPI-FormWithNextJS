2753a8f25cca76d20c4f99aa3a80954e745c9d7e
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-06 12:45:11 +0300

    components page created

---
+++ src/app/components/page.tsx
@@ -0,0 +1,7 @@
+export default function Modules() {
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            Components
+        </section>
+    )
+}
